992112f49e8f2ff2ef264cb76e6102e035097a9f
Sarah Teoh <you@example.com>
2020-07-07 12:55:53 +0800

    Add Japanese comments for assignment

--- aes_encrypt.py
+++ aes_encrypt.py
@@ -17,12 +17,13 @@ Sbox = [0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 
         0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
         0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
         0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16]
-        
+       
+# Yield successive n-sized chunks from lst.
 def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+# Rotate shift a list by n
 def rotate(l, n):
     return l[n:] + l[:n]
 
@@ -57,8 +58,6 @@ def generateT2T3(g):
 			if A * 2 <= 255:
 				T2[i + 4 * j] = A * 2 
 			else:
-				#print("i + 4 * j: ", i + 4 * j)
-				#print(A*2)
 				T2[i + 4 * j] = (A * 2) ^ 283
 
 			T3[i + 4 * j] = A ^ T2[i + 4 * j]
@@ -115,12 +114,12 @@ print("f: ", f)
 
 g = ShiftRows(f)
 g = list(chain(*g)) # Flatten multiple lists into one list
-g = [int(num, 16) for num in g]
+g = [int(num, 16) for num in g] 
 print("g: ", g)
-
 
 h = MixColumns(g)
 print("h: ", h)
 
 encrypted = [chr(dec) for dec in h]
+
 print("Encrypted: ", encrypted)
